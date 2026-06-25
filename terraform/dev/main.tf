d7cb3c28fb2e857703e91d5fe182b2632a895499
arsh-e-noor <arshkhan4877@gmail.com>
2026-06-26 02:00:43 +0500

    EC2 files

---
+++ terraform/dev/main.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+  source = "../modules/vpc"
+
+  project_name = var.project_name
+
+  vpc_cidr = var.vpc_cidr
+
+  public_subnet_1_cidr = var.public_subnet_1_cidr
+  public_subnet_2_cidr = var.public_subnet_2_cidr
+
+  private_subnet_1_cidr = var.private_subnet_1_cidr
+  private_subnet_2_cidr = var.private_subnet_2_cidr
+}
